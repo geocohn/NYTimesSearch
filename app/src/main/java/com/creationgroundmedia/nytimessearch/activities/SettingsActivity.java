6192cc4d476dd43a89370f57f7d87f2d8088480e
geocohn <geo@creationgroundmedia.com>
2016-10-23 17:41:42 -0700

    feat: Add up navigation

--- app/src/main/java/com/creationgroundmedia/nytimessearch/activities/SettingsActivity.java
+++ app/src/main/java/com/creationgroundmedia/nytimessearch/activities/SettingsActivity.java
@@ -18,7 +18,9 @@ package com.creationgroundmedia.nytimessearch.activities;
 
 import android.os.Bundle;
 import android.preference.PreferenceFragment;
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.ActionBar;
+import android.view.MenuItem;
 
 import com.creationgroundmedia.nytimessearch.R;
 
@@ -39,6 +41,17 @@ public class SettingsActivity extends AppCompatPreferenceActivity
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
         getFragmentManager().beginTransaction().replace(android.R.id.content, new MyPreferenceFragment()).commit();
+    }
+    @Override
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            // Respond to the action bar's Up/Home button
+            case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     public static class MyPreferenceFragment extends PreferenceFragment
